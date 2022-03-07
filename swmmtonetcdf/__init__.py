b4eff234772762bdead08a21963d2fb2739c902a
cbuahin <calebgh@gmail.com>
2022-03-07 17:27:20 -0500

    First release

--- swmmtonetcdf/__init__.py
+++ swmmtonetcdf/__init__.py
@@ -1,9 +1,9 @@
 from swmmtonetcdf.swmmtonetcdf import *
 
-VERSION_INFO = (0, 0, 0, 'dev0')
+VERSION_INFO = (0, 0, 0)
 
 __version__ = '.'.join(map(str, VERSION_INFO))
-__author__ = 'Caleb A. Buahin'
+__author__ = 'Caleb Buahin and Jenn Wu'
 __copyright__ = 'Copyright (c) 2022 Caleb A. Buahin'
 __licence__ = 'GNU GPLv3'
 __all__ = []
