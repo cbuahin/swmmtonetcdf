bad700a80f335a1975d41a4f2b883a6d9bef54db
Jennifer Wu <wuu.jennifer@gmail.com>
2022-03-07 15:47:34 -0500

    Set up as per PyPI package instructions

--- swmmtonetcdf/__init__.py
+++ swmmtonetcdf/__init__.py
@@ -1,3 +1,10 @@
-
-
 from swmmtonetcdf.swmmtonetcdf import *
+
+
+VERSION_INFO = (0, 0, 0, 'dev0')
+
+__version__ = '.'.join(map(str, VERSION_INFO))
+__author__ = 'Caleb A. Buahin'
+__copyright__ = 'Copyright (c) 2022 Caleb A. Buahin'
+__licence__ = 'GNU GPLv3'
+__all__ = []
