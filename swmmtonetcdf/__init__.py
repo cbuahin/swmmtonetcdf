5b99d21a3de6965bd25d70a1c1afd1e9d18613c3
cbuahin <calebgh@gmail.com>
2022-03-08 10:33:20 -0500

    Release version v0.0.3

--- swmmtonetcdf/__init__.py
+++ swmmtonetcdf/__init__.py
@@ -1,7 +1,7 @@
 from swmmtonetcdf.swmmtonetcdf import *
 from swmmtonetcdf.__main__ import main
 
-VERSION_INFO = (0, 0, 0)
+VERSION_INFO = (0, 0, 3)
 
 __version__ = '.'.join(map(str, VERSION_INFO))
 __author__ = 'Caleb Buahin and Jenn Wu'
